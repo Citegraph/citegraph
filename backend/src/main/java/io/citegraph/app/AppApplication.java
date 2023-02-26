5c26e9570ea4d7fc4a8ee1dd30b96a0a7e03a338
Boxuan Li <liboxuan@connect.hku.hk>
2023-02-26 12:22:59 -0800

    Add spring-boot start up project

---
+++ backend/src/main/java/io/citegraph/app/AppApplication.java
@@ -0,0 +1,13 @@
+package io.citegraph.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
