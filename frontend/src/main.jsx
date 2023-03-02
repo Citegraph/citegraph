ef212e9c0224ca86efdc2bbf8395a4e4ed938957
Boxuan Li <liboxuan@connect.hku.hk>
2023-03-01 22:24:58 -0800

    Follow reactrouter tutorial

---
+++ frontend/src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+        {
+          path: "contacts/:contactId",
+          element: <Contact />,
+        },
+      ],
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ frontend/src/routes/root.jsx
@@ -0,0 +1,47 @@
+import { Outlet, Link } from "react-router-dom";
+
+export default function Root() {
+    return (
+      <>
+        <div id="sidebar">
+          <h1>React Router Contacts</h1>
+          <div>
+            <form id="search-form" role="search">
+              <input
+                id="q"
+                aria-label="Search contacts"
+                placeholder="Search"
+                type="search"
+                name="q"
+              />
+              <div
+                id="search-spinner"
+                aria-hidden
+                hidden={true}
+              />
+              <div
+                className="sr-only"
+                aria-live="polite"
+              ></div>
+            </form>
+            <form method="post">
+              <button type="submit">New</button>
+            </form>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <Link to={`contacts/1`}>Your Name</Link>
+              </li>
+              <li>
+                <Link to={`contacts/2`}>Your Friend</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div id="detail">
+          <Outlet />
+        </div>
+      </>
+    );
+  }
