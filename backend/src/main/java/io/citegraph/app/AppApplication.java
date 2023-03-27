de2610ecdc3df8ae3d1a2c51a80ee0da2d52a97b
Boxuan Li <liboxuan@connect.hku.hk>
2023-03-27 00:09:55 -0700

    Redirect all pages to index.html for SPA to work

--- backend/src/main/java/io/citegraph/app/AppApplication.java
+++ backend/src/main/java/io/citegraph/app/AppApplication.java
@@ -2,12 +2,22 @@ package io.citegraph.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication implements ErrorController {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+	private static final String PATH = "/error";
+
+	@RequestMapping(value = PATH)
+	public String error() {
+		return "forward:/index.html";
+	}
 }
