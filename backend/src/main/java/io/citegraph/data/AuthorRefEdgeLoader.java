dd0809ef5f8c9131261189620255715b1db634e0
Boxuan Li <liboxuan@connect.hku.hk>
2023-04-20 22:58:40 +0800

    Implement AuthorRefEdgeLoader Spark App that creates author-author edges

--- backend/src/main/java/io/citegraph/data/AuthorRefEdgeLoader.java
+++ backend/src/main/java/io/citegraph/data/AuthorRefEdgeLoader.java
@@ -9,9 +9,24 @@ import org.apache.spark.launcher.SparkLauncher;
 import org.apache.spark.serializer.KryoSerializer;
 import org.apache.tinkerpop.gremlin.hadoop.Constants;
 import org.apache.tinkerpop.gremlin.hadoop.structure.HadoopGraph;
+import org.apache.tinkerpop.gremlin.hadoop.structure.io.VertexWritable;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
 import org.apache.tinkerpop.gremlin.spark.structure.io.SparkIOUtil;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.structure.util.star.StarGraph;
+import org.janusgraph.core.JanusGraph;
+import org.janusgraph.core.JanusGraphFactory;
 import org.janusgraph.hadoop.formats.cql.CqlInputFormat;
 import org.janusgraph.hadoop.serialize.JanusGraphKryoRegistrator;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static io.citegraph.app.GraphConfiguration.GRAPH_CONFIG_NAME;
 
 public class AuthorRefEdgeLoader {
     public static void main(String[] args) {
@@ -30,8 +45,48 @@ public class AuthorRefEdgeLoader {
         sparkGraphConfiguration.setProperty("janusgraphmr.ioformat.conf.storage.cql.keyspace", "janusgraph");
         sparkGraphConfiguration.setProperty("cassandra.input.partitioner.class", "org.apache.cassandra.dht.Murmur3Partitioner");
 
+        URL resource = GraphInitializer.class.getClassLoader().getResource(GRAPH_CONFIG_NAME);
+
         // load vertices
-        JavaPairRDD verticesRDD = SparkIOUtil.loadVertices(sparkGraphConfiguration, sparkContext);
+        JavaPairRDD<Object, VertexWritable> verticesRDD = SparkIOUtil.loadVertices(sparkGraphConfiguration, sparkContext);
+        verticesRDD.values().foreachPartition(partition -> {
+            // SparkRDDs are immutable, so we have to use normal JanusGraph transactions to
+            // write updated data back to JanusGraph. For each partition, we open a transaction.
+            JanusGraph graph;
+            try {
+                graph = JanusGraphFactory.open(resource.toURI().getPath());
+            } catch (Exception ex) {
+                System.out.println(ex);
+                return;
+            }
+
+            JanusGraph finalGraph = graph;
+            partition.forEachRemaining(
+                vertexWritable -> {
+                    GraphTraversalSource g = finalGraph.traversal();
+                    StarGraph.StarVertex v = vertexWritable.get();
+                    if (!Objects.equals(v.value("type"), "author")) return;
+                    String name = v.value("name");
+                    List<Vertex> referees = g.V(v.id()).out("writes").out("cites").in("writes").toList();
+                    if (referees.isEmpty()) return;
+                    Map<Vertex, Integer> refereeToCounter = new HashMap<>();
+                    for (Vertex referee : referees) {
+                        refereeToCounter.put(referee, refereeToCounter.getOrDefault(referee, 0) + 1);
+                    }
+                    // System.out.println("Author " + v.id() + " cites " + referees.size() + " authors, after dedup = " + refereeToCounter.size());
+                    Vertex fromV = g.V(v.id()).next();
+                    for (Map.Entry<Vertex, Integer> entry : refereeToCounter.entrySet()) {
+                        if (!g.V(fromV).outE().where(__.otherV().is(entry.getKey())).hasNext()) {
+                            g.addE("refers").from(fromV).to(entry.getKey())
+                                .property("refCount", entry.getValue())
+                                .property("name", name)
+                                .next();
+                        }
+                    }
+                    g.tx().commit();
+                });
+            graph.close();
+        });
         System.out.println("number of RDDs:" + verticesRDD.count());
     }
 }
