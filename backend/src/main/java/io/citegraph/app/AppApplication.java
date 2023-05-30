4dc874d4834f0f0d6bac86de7492e9891befa493
Boxuan Li <liboxuan@connect.hku.hk>
2023-05-29 21:35:05 -0700

    Fix page render status code 404 bug
    The only pitfall is if the page is like
    /author/xxx, or /paper/xxx, then no matter
    if that suffix is valid or not, the page status
    will always be 200, even if it renders an error
    page.

--- backend/src/main/java/io/citegraph/app/AppApplication.java
+++ backend/src/main/java/io/citegraph/app/AppApplication.java
@@ -2,22 +2,13 @@ package io.citegraph.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @SpringBootApplication
-public class AppApplication implements ErrorController {
+public class AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
-
-	private static final String PATH = "/error";
-
-	@RequestMapping(value = PATH)
-	public String error() {
-		return "forward:/index.html";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 }
