3f2764ee3d77175526e5aebfbb736a6f2f3f8869
Boxuan Li <liboxuan@connect.hku.hk>
2023-06-24 16:35:13 -0700

    Remove unnecessary favicon include in header

--- frontend/app/root.jsx
+++ frontend/app/root.jsx
@@ -77,7 +77,6 @@ export default function Root() {
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Links />
         <title>Citegraph</title>
