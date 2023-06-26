70efcbce3d1746f057de11a42f6496ee4822c7ce
Boxuan Li <liboxuan@connect.hku.hk>
2023-06-25 21:25:50 -0700

    [UI] Fix backend url in prod

--- frontend/app/apis/commons.js
+++ frontend/app/apis/commons.js
@@ -1,3 +1,6 @@
-export const API_URL = "http://localhost:8080/apis";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "/apis"
+    : "http://localhost:8080/apis";
 export const DEFAULT_SEARCH_LIMIT = 100;
 export const MAX_SEARCH_LIMIT = 1000;
