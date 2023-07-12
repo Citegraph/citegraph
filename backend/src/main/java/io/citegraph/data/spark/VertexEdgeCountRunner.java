f3b349a31c12a8da4bf16090ca6506753aed1456
Boxuan Li <liboxuan@connect.hku.hk>
2023-07-11 19:08:25 -0700

    [backend] Count vertices and edges by type

--- backend/src/main/java/io/citegraph/data/spark/VertexEdgeCountRunner.java
+++ backend/src/main/java/io/citegraph/data/spark/VertexEdgeCountRunner.java
@@ -3,22 +3,32 @@ package io.citegraph.data.spark;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.spark.process.computer.SparkGraphComputer;
 import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.T;
 import org.apache.tinkerpop.gremlin.structure.util.GraphFactory;
+
+import java.util.Map;
 
 import static io.citegraph.data.spark.Utils.getSparkGraphConfig;
 
 /**
  * This Spark application counts number of vertices and edges stored in the graph
  * by their types
+ *
+ * 07/11/2023:
+ * number of vertices by type is: {paper=5259858, author=4116015}
+ * number of edges by type is: {collaborates=37588754, cites=32724316, refers=224949068, writes=16863646}
  */
 public class VertexEdgeCountRunner {
     public static void main(String[] args) throws Exception {
         Graph graph = GraphFactory.open(getSparkGraphConfig());
         GraphTraversalSource g = graph.traversal().withComputer(SparkGraphComputer.class);
         long startTime = System.currentTimeMillis();
-        long numV = g.V().count().next();
+        Map<Object, Long> numOfVerticesByType = g.V().groupCount().by("type").next();
+        Map<Object, Long> numOfEdgesByLabel = g.E().groupCount().by(T.label).next();
         long duration = (System.currentTimeMillis() - startTime) / 1000;
-        System.out.println("number of vertices = " + numV + ", elapsed time = " + duration + " seconds.");
+        System.out.println("number of vertices by type is: " + numOfVerticesByType);
+        System.out.println("number of edges by type is: " + numOfEdgesByLabel);
+        System.out.println("elapsed time = " + duration + " seconds");
         graph.close();
     }
 }
