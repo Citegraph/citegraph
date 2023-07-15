2c30b9c988dfd896a0192e450cc786015105833f
Boxuan Li <liboxuan@connect.hku.hk>
2023-07-15 14:37:27 -0700

    [UI] Add Google Analytics script

--- frontend/app/root.jsx
+++ frontend/app/root.jsx
@@ -100,6 +100,21 @@ export default function Root() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Meta />
         <Links />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z7LN8421SX"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Z7LN8421SX');
+  `,
+          }}
+        ></script>
       </head>
       <body>
         <Scripts />
