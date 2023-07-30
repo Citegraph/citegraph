6acaa31bb513f46125ec9a5a119cf68651dc58ed
Boxuan Li <liboxuan@connect.hku.hk>
2023-07-30 12:54:53 -0700

    [UI] Remove footer
    It has caused a lot trouble and display issues so far

--- frontend/app/root.jsx
+++ frontend/app/root.jsx
@@ -14,11 +14,10 @@ import stylesheetUrl from "./index.css";
 import { API_URL } from "./apis/commons";
 import { Divider } from "antd";
 import { MenuOutlined } from "@ant-design/icons";
-import { Layout, Select, Input, Space } from "antd";
+import { Select, Input, Space } from "antd";
 
 const { Option } = Select;
 const { Search } = Input;
-const { Footer } = Layout;
 
 export const links = () => {
   return [
@@ -119,122 +118,116 @@ export default function Root() {
       <body>
         <Scripts />
         <div id="root">
-          <Layout className="main-layout">
-            <div id="hamburger-container">
-              <button className="hamburger" onClick={toggleSidebar}>
-                <MenuOutlined />
-              </button>
-            </div>
-            <div
-              id="sidebar-container"
-              className={isSidebarActive ? "active" : ""}
-            >
-              <div id="sidebar">
-                <Link to="/">
-                  <img src={logo} id="logo" alt="Logo" />
-                </Link>
-                <div>
-                  <form id="search-form" role="search">
-                    <Space.Compact>
-                      <Select
-                        defaultValue="author"
-                        style={{ width: 80 }}
-                        onChange={handleSearchTypeChange}
-                      >
-                        <Option value="author">Name</Option>
-                        <Option value="paper">Title</Option>
-                      </Select>
-                      <Search
-                        placeholder={placeholder}
-                        onChange={handleSearch}
-                        onSearch={handleSearch}
-                        loading={loading}
-                        allowClear
-                      />
-                    </Space.Compact>
-                  </form>
-                </div>
-                <nav>
-                  {searchResults.length ? (
-                    <ul>
-                      {searchResults.map((result) =>
-                        result.name ? (
-                          <li key={result.id}>
-                            <Link to={`author/${result.id}`}>
-                              {result.name}
-                            </Link>
-                          </li>
-                        ) : (
-                          <li key={result.id}>
-                            <Link to={`paper/${result.id}`}>
-                              {result.title}
-                            </Link>
-                          </li>
-                        )
-                      )}
-                    </ul>
-                  ) : (
-                    query.length > 0 && (
-                      <ul>
-                        <li>
-                          <i>No results</i>
-                        </li>
-                      </ul>
-                    )
-                  )}
-                  {query.length > 0 && <Divider />}
-                  <p>
-                    <b>People are searching</b>
-                  </p>
-                  {authors.length ? (
-                    <ul>
-                      {authors.map((author) => (
-                        <li key={author.id}>
-                          <Link to={`author/${author.id}`}>{author.name}</Link>
-                        </li>
-                      ))}
-                    </ul>
-                  ) : (
-                    <p>
-                      <i>No authors</i>
-                    </p>
-                  )}
-                </nav>
+          <div id="hamburger-container">
+            <button className="hamburger" onClick={toggleSidebar}>
+              <MenuOutlined />
+            </button>
+          </div>
+          <div
+            id="sidebar-container"
+            className={isSidebarActive ? "active" : ""}
+          >
+            <div id="sidebar">
+              <Link to="/">
+                <img src={logo} id="logo" alt="Logo" />
+              </Link>
+              <div>
+                <form id="search-form" role="search">
+                  <Space.Compact>
+                    <Select
+                      defaultValue="author"
+                      style={{ width: 80 }}
+                      onChange={handleSearchTypeChange}
+                    >
+                      <Option value="author">Name</Option>
+                      <Option value="paper">Title</Option>
+                    </Select>
+                    <Search
+                      placeholder={placeholder}
+                      onChange={handleSearch}
+                      onSearch={handleSearch}
+                      loading={loading}
+                      allowClear
+                    />
+                  </Space.Compact>
+                </form>
               </div>
+              <nav>
+                {searchResults.length ? (
+                  <ul>
+                    {searchResults.map((result) =>
+                      result.name ? (
+                        <li key={result.id}>
+                          <Link to={`author/${result.id}`}>{result.name}</Link>
+                        </li>
+                      ) : (
+                        <li key={result.id}>
+                          <Link to={`paper/${result.id}`}>{result.title}</Link>
+                        </li>
+                      )
+                    )}
+                  </ul>
+                ) : (
+                  query.length > 0 && (
+                    <ul>
+                      <li>
+                        <i>No results</i>
+                      </li>
+                    </ul>
+                  )
+                )}
+                {query.length > 0 && <Divider />}
+                <p>
+                  <b>People are searching</b>
+                </p>
+                {authors.length ? (
+                  <ul>
+                    {authors.map((author) => (
+                      <li key={author.id}>
+                        <Link to={`author/${author.id}`}>{author.name}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>
+                    <i>No authors</i>
+                  </p>
+                )}
+              </nav>
             </div>
-            <div id="detail">
-              <Outlet />
-            </div>
-            <Footer id="footer" className="footer-component">
-              CiteGraph is open-sourced on{" "}
-              <a
-                href="https://github.com/li-boxuan/citegraph"
-                target="_blank"
-                rel="noreferrer"
-                className="no-underline"
-              >
-                GitHub
-              </a>
-              , powered by{" "}
-              <a
-                href="https://janusgraph.org/"
-                target="_blank"
-                rel="noreferrer"
-                className="no-underline"
-              >
-                JanusGraph
-              </a>{" "}
-              and{" "}
-              <a
-                href="https://www.aminer.org/citation/"
-                target="_blank"
-                rel="noreferrer"
-                className="no-underline"
-              >
-                DBLP-Citation-network V14
-              </a>
-            </Footer>
-          </Layout>
+          </div>
+          <div id="detail">
+            <Outlet />
+          </div>
+          {/* <Footer id="footer" className="footer-component">
+            CiteGraph is open-sourced on{" "}
+            <a
+              href="https://github.com/li-boxuan/citegraph"
+              target="_blank"
+              rel="noreferrer"
+              className="no-underline"
+            >
+              GitHub
+            </a>
+            , powered by{" "}
+            <a
+              href="https://janusgraph.org/"
+              target="_blank"
+              rel="noreferrer"
+              className="no-underline"
+            >
+              JanusGraph
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://www.aminer.org/citation/"
+              target="_blank"
+              rel="noreferrer"
+              className="no-underline"
+            >
+              DBLP-Citation-network V14
+            </a>
+          </Footer> */}
         </div>
       </body>
     </html>
