db32e9b9aece2056c2f7a3a4461f846ac54dbb98
Boxuan Li <liboxuan@connect.hku.hk>
2023-07-30 15:54:11 -0700

    [UI] Remove antd menu component and use native ones

--- frontend/app/header.jsx
+++ frontend/app/header.jsx
@@ -1,27 +1,14 @@
 import { Link } from "@remix-run/react";
 import logo from "./assets/logo.svg";
-import { Select, Input, Space, Menu } from "antd";
+import { Select, Input, Space } from "antd";
 
 const { Option } = Select;
 const { Search } = Input;
 
 export default function Header() {
-  const items = [
-    {
-      label: "Navigation One",
-      key: "mail",
-    },
-    {
-      label: "Navigation Two",
-      key: "app",
-      disabled: true,
-    },
-  ];
-
   return (
-    <div style={{ display: "flex", justifyContent: "space-between" }}>
-      {/* Left section for logo */}
-      <div style={{ padding: "16px 0" }}>
+    <div id="header">
+      <div id="logo">
         <Link to="/">
           <img
             src={logo}
@@ -31,8 +18,7 @@ export default function Header() {
         </Link>
       </div>
 
-      {/* Center section for search bar */}
-      <div style={{ alignSelf: "center" }}>
+      <div id="search">
         <Input.Search
           placeholder="input search text"
           onSearch={(value) => console.log(value)}
@@ -40,8 +26,7 @@ export default function Header() {
         />
       </div>
 
-      {/* Right section for other menu items */}
-      <Menu mode="horizontal" items={items} />
+      <div id="menu-items"></div>
     </div>
   );
 }
