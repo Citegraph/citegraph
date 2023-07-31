816b8171800f383457e06b5739cc34cd82cf7931
Boxuan Li <liboxuan@connect.hku.hk>
2023-07-30 20:46:48 -0700

    [UI] Add menu items CSS

--- frontend/app/header.jsx
+++ frontend/app/header.jsx
@@ -90,7 +90,31 @@ export default function Header() {
         </Space.Compact>
       </div>
 
-      <div id="menu-items"></div>
+      <div id="menu-items">
+        <Link to="/about" className="menu-item">
+          About
+        </Link>
+        <a
+          href="https://github.com/Citegraph/citegraph"
+          target="_blank"
+          rel="noreferrer"
+          className="menu-item"
+        >
+          GitHub
+          <svg
+            width="13.5"
+            height="13.5"
+            aria-hidden="true"
+            viewBox="0 0 24 24"
+            className="external-link-icon"
+          >
+            <path
+              fill="currentColor"
+              d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"
+            ></path>
+          </svg>
+        </a>
+      </div>
     </div>
   );
 }
