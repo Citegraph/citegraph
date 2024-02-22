73645a5fb2dc6edcba4dd37331fcdd4e28df0cc3
Boxuan Li <liboxuan@connect.hku.hk>
2024-02-21 20:54:48 -0800

    Reduce default search limit from 200 to 100

--- frontend/app/apis/commons.js
+++ frontend/app/apis/commons.js
@@ -5,8 +5,8 @@ export const API_URL =
   process.env.NODE_ENV === "production"
     ? "https://www.citegraph.io/apis"
     : "http://localhost:8080/apis";
-export const DEFAULT_SEARCH_LIMIT = 200;
-export const MAX_SEARCH_LIMIT = 2000;
+export const DEFAULT_SEARCH_LIMIT = 100;
+export const MAX_SEARCH_LIMIT = 1000;
 
 // table pagination setting: how many rows to show in each page
 export const DEFAULT_PAGE_SIZE = 50;
